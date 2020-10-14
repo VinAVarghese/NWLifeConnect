ce346ef3821f1e079c47a62b44b5ef48d6ef5e01
Vinny V <VinAVarghese@gmail.com>
2020-10-14 08:48:02 -0700

    correcting hero link

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <>
             <div className="hero">
-                <a href="https://www.google.com/" rel="noopener noreferrer" target= "_blank">
+                <a href="https://nwlife.church/" rel="noopener noreferrer" target= "_blank">
                     <img className="logo" src="./assets/logos/nwlifelogo.png" alt="logo"/>
                 </a>
                 <div className="filter"></div>
