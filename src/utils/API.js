6535bee71aa2f32e9c8713b9b4101b82a336a96a
Vins MacBook <vinavarghese@gmail.com>
2020-10-15 14:41:20 -0700

    adjusting heroku deployment settings

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // Change when locally testing and deploying
-const urlPrefix = "https://nwlife-connect-server.herokuapp.com";
+const urlPrefix = "http://nwlife-connect-server.herokuapp.com";
 // const urlPrefix = "http://localhost:8080";
 
 
